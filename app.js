f779f8eeb6e5ff7549656f2df507f133194c2473
Raúl Jiménez <jimenezjrs@gmail.com>
2016-08-11 18:51:01 -0500

    Abstracting the main functionality

--- app.js
+++ app.js
@@ -1,1 +1,89 @@
-const app = require('./config/app')
+// const app = require('./config/app')
+const express = require('express')
+const bodyParser = require('body-parser')
+const https = require('https')
+
+const {
+  APP_SECRET,
+  VALIDATION_TOKEN,
+  verifyRequestSignature,
+  receivedAuthentication,
+  receivedMessage,
+  receivedDeliveryConfirmation,
+  receivedPostback,
+  receivedMessageRead
+} = require('./config/app')
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else if (messagingEvent.read) {
+          receivedMessageRead(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+});
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
--- config/app.js
+++ config/app.js
@@ -11,9 +11,9 @@
 'use strict';
 
 const
-  bodyParser = require('body-parser'),
+  // bodyParser = require('body-parser'),
   crypto = require('crypto'),
-  express = require('express'),
+  // express = require('express'),
   https = require('https');
 
 const {
@@ -35,72 +35,72 @@ const {
 
 const {APP_SECRET, VALIDATION_TOKEN} = require('./bootstrap')
 
-var app = express();
-
-app.set('port', process.env.PORT || 5000);
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(express.static('public'));
-
-/*
- * Use your own validation token. Check that the token used in the Webhook
- * setup is the same token used here.
- *
- */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page.
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          receivedPostback(messagingEvent);
-        } else if (messagingEvent.read) {
-          receivedMessageRead(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
+// var app = express();
+//
+// app.set('port', process.env.PORT || 5000);
+// app.use(bodyParser.json({ verify: verifyRequestSignature }));
+// app.use(express.static('public'));
+//
+// /*
+//  * Use your own validation token. Check that the token used in the Webhook
+//  * setup is the same token used here.
+//  *
+//  */
+// app.get('/webhook', function(req, res) {
+//   if (req.query['hub.mode'] === 'subscribe' &&
+//       req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+//     console.log("Validating webhook");
+//     res.status(200).send(req.query['hub.challenge']);
+//   } else {
+//     console.error("Failed validation. Make sure the validation tokens match.");
+//     res.sendStatus(403);
+//   }
+// });
+//
+//
+// /*
+//  * All callbacks for Messenger are POST-ed. They will be sent to the same
+//  * webhook. Be sure to subscribe your app to your page to receive callbacks
+//  * for your page.
+//  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+//  *
+//  */
+// app.post('/webhook', function (req, res) {
+//   var data = req.body;
+//
+//   // Make sure this is a page subscription
+//   if (data.object == 'page') {
+//     // Iterate over each entry
+//     // There may be multiple if batched
+//     data.entry.forEach(function(pageEntry) {
+//       var pageID = pageEntry.id;
+//       var timeOfEvent = pageEntry.time;
+//
+//       // Iterate over each messaging event
+//       pageEntry.messaging.forEach(function(messagingEvent) {
+//         if (messagingEvent.optin) {
+//           receivedAuthentication(messagingEvent);
+//         } else if (messagingEvent.message) {
+//           receivedMessage(messagingEvent);
+//         } else if (messagingEvent.delivery) {
+//           receivedDeliveryConfirmation(messagingEvent);
+//         } else if (messagingEvent.postback) {
+//           receivedPostback(messagingEvent);
+//         } else if (messagingEvent.read) {
+//           receivedMessageRead(messagingEvent);
+//         } else {
+//           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+//         }
+//       });
+//     });
+//
+//     // Assume all went well.
+//     //
+//     // You must send back a 200, within 20 seconds, to let us know you've
+//     // successfully received the callback. Otherwise, the request will time out.
+//     res.sendStatus(200);
+//   }
+// });
 
 /*
  * Verify that the callback came from Facebook. Using the App Secret from
@@ -362,11 +362,20 @@ function receivedMessageRead(event) {
     "number %d", watermark, sequenceNumber);
 }
 
-// Start server
-// Webhooks must be available via SSL with a certificate signed by a valid
-// certificate authority.
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// // Start server
+// // Webhooks must be available via SSL with a certificate signed by a valid
+// // certificate authority.
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
-module.exports = app;
+module.exports = {
+  APP_SECRET,
+  VALIDATION_TOKEN,
+  verifyRequestSignature,
+  receivedAuthentication,
+  receivedMessage,
+  receivedDeliveryConfirmation,
+  receivedPostback,
+  receivedMessageRead
+}
